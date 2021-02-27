63550b5ce2fb96812dd9011e251f9fe6d8c175e3
Kevin Dillon <39660190+kevin-dillon@users.noreply.github.com>
2021-02-26 20:08:31 -0800

    Added Bulma

--- echocarbon/src/App.js
+++ echocarbon/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
-
-import React from "react";
 import "bulma/css/bulma.css"
+
 
 function App() {
   return (
     <div className="App">
-      <a class="button is-primary">
-        Button
-      </a>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          EchoCarbon
-        </p>
+        <p>EchoCarbon</p>
+        <div class="container is-widescreen">
+          <div class="notification is-primary">
+            Carbon Footprint Calculator
+            <progress class="progress" value="60" max="100">60%</progress>
+              <article class="message is-success">
+                <div class="message-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+                </div>
+              </article>
+              <input class="input is-success" type="text" placeholder="Type here!"></input>
+          </div>
+        </div>
       </header>
     </div>
-    
   );
 }
 
