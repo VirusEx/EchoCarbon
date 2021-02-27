c846c0e778f570d5d8507d0a3cdfb0e11eb60b48
ecorona9 <ecorona9@csu.fullerton.edu>
2021-02-26 18:12:10 -0800

    Did 'npm install bulma' and added 2 imports on App.js

--- echocarbon/src/App.js
+++ echocarbon/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import React from "react";
+import "bulma/css/bulma.css"
+
 function App() {
   return (
     <div className="App">
+      <a class="button is-primary">
+        Button
+      </a>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -11,6 +18,7 @@ function App() {
         </p>
       </header>
     </div>
+    
   );
 }
 
