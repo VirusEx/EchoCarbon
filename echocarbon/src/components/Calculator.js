737ea4642a05dd3ac654b274abe59dcb0b497e0b
Kevin Dillon <39660190+kevin-dillon@users.noreply.github.com>
2021-02-27 20:11:08 -0800

    Separated DropDown to Calculator.js

---
+++ echocarbon/src/components/Calculator.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class DropDown extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: 'DIESEL'};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('YOUR VEHICLE TYPE: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Pick your type of vehicle:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="DIESEL">DIESEL</option>
+                <option value="PETROL">PETROL</option>
+                <option value="ELETRIC">ELETRIC</option>
+              </select>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
+}
+
+export default DropDown;
