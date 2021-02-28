5e2fd5ad80acc92f7b501a7dd62bd7e21aea8999
ecorona9 <ecorona9@csu.fullerton.edu>
2021-02-27 20:01:16 -0800

    Added DropDown class from React https://reactjs.org/docs/forms.html

--- echocarbon/src/App.js
+++ echocarbon/src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
 import "bulma/css/bulma.css"
+import React from 'react';
+
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'DIESEL'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('YOUR VEHICLE TYPE: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your type of vehicle:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="DIESEL">DIESEL</option>
+            <option value="PETROL">PETROL</option>
+            <option value="ELETRIC">ELETRIC</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        
         <p class = "is-size-1">EchoCarbon</p>
         <div class="container is-widescreen">
           <div class="notification is-success">
@@ -15,7 +54,9 @@ function App() {
                 <div class="message-body">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
                 </div>
+                
               </article>
+              <DropDown />
               <input class="input is-success" type="text" placeholder="Type here!"></input>
           </div>
         </div>
