07d3b5e392e9a15b2c8f88980705991d7fcad1d1
Kevin Dillon <39660190+kevin-dillon@users.noreply.github.com>
2021-03-06 18:18:01 -0800

    Fixed indentation in DataForm.js

--- echocarbon/src/components/DataForm.js
+++ echocarbon/src/components/DataForm.js
@@ -32,25 +32,21 @@ class DataForm extends React.Component {
             <div class="box is-light label">Weekly miles:</div>
             <br></br>
           </div>
-          <div class = "columns is-centered">
-            <input class="column is-half has-text-centered" type="text" placeholder="Text input"
+          <div class="columns is-centered">
+            <input
+              class="column is-half has-text-centered"
+              type="text"
+              placeholder="Text input"
               name="weeklyMiles"
               type="number"
               min="0"
               value={this.state.weeklyMiles}
-              onChange={this.handleInputChange}
-            />
+              onChange={this.handleInputChange} />
           </div>
-          <div>
-          {/* {this.state.weeklyMiles <= 150 ?
-            <div class="notification is-info">Congrats! You don't drive that many miles per week. <br></br> You do not put a lot of pollution into the air.</div> :
-            <div class="notification is-info">Sorry, you are driving too many miles per week. <br></br> You put a lot of pollution into the air.</div>
-          } */}
-        </div>
 
-        <br></br>
+          <br></br>
         
-        <div class="columns is-centered">
+          <div class="columns is-centered">
             <div class="box is-light label">Car MPG:</div>
             <br></br>
           </div>
@@ -60,21 +56,16 @@ class DataForm extends React.Component {
               type="number"
               min="1"
               value={this.state.mpg}
-              onChange={this.handleInputChange}
-            />
+              onChange={this.handleInputChange} />
           </div>
           <div>
-          {
-          ((this.state.weeklyMiles/this.state.mpg) * 19.60) <= 19.60 ?
-            <div class="notification is-info"> Congrats! You don't drive that many miles per week. <br></br> You do not put a lot of pollution into the air. <br></br>Pounds of CO2 released per week: {((this.state.weeklyMiles/this.state.mpg) * 19.60).toFixed(2)}</div> :
-            <div class="notification is-info"> Sorry, you are driving too many miles per week. <br></br> You put a lot of pollution into the air. <br></br>Pounds of CO2 released per week: {((this.state.weeklyMiles/this.state.mpg) * 19.60).toFixed(2)}</div>
-          }
+            {
+            ((this.state.weeklyMiles/this.state.mpg) * 19.60) <= 19.60 ?
+              <div class="notification is-info"> Congrats! You don't drive that many miles per week. <br></br> You do not put a lot of pollution into the air. <br></br>Pounds of CO2 released per week: {((this.state.weeklyMiles/this.state.mpg) * 19.60).toFixed(2)}</div> :
+              <div class="notification is-info"> Sorry, you are driving too many miles per week. <br></br> You put a lot of pollution into the air. <br></br>Pounds of CO2 released per week: {((this.state.weeklyMiles/this.state.mpg) * 19.60).toFixed(2)}</div>
+            }
+          </div>
         </div>
-        </div>
-
-
-
-
       </div>
     );
   }
