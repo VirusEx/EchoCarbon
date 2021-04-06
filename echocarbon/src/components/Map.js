db6dbf5d079d1cde469c101aa39e9259ae0aa687
Kevin Dillon <39660190+kevin-dillon@users.noreply.github.com>
2021-04-06 12:37:22 -0700

    added maps

--- echocarbon/src/components/Map.js
+++ echocarbon/src/components/Map.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import { WorldMap } from "react-svg-worldmap"
 import { Link } from 'react-router-dom';
 
 class Map extends React.Component {
+  data =
+    [
+      { country: "cn", value: 1389618778 }, // china
+      { country: "in", value: 1311559204 }, // india
+      { country: "us", value: 331883986 },  // united states
+      { country: "id", value: 264935824 },  // indonesia
+      { country: "pk", value: 210797836 },  // pakistan
+      { country: "br", value: 210301591 },  // brazil
+      { country: "ng", value: 208679114 },  // nigeria
+      { country: "bd", value: 161062905 },  // bangladesh
+      { country: "ru", value: 141944641 },  // russia
+      { country: "mx", value: 127318112 }   // mexico
+    ];
+
   constructor(props) {
     super(props);
     this.state = {
@@ -21,12 +36,17 @@ class Map extends React.Component {
     event.preventDefault();
   }
 
+  
+
   render() {
+
       return (
+        
         <div className="">
           <div class="box">
             Map
           </div>
+          <WorldMap color="green" title="Top 10 Populous Countries" value-suffix="people" size="xxl" data={this.data} />
         </div>
       );
     }
