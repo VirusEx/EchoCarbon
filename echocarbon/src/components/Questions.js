4a4e1e47736cca1acae349c96bfe929d75fc2487
Kevin Dillon <39660190+kevin-dillon@users.noreply.github.com>
2021-04-11 17:22:32 -0700

    Updated ticket creator

--- echocarbon/src/components/Questions.js
+++ echocarbon/src/components/Questions.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import emailjs from "emailjs-com"
 import { Link } from 'react-router-dom';
 
+
 class Questions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      ticketMessage: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -14,81 +15,65 @@ class Questions extends React.Component {
   }
 
   handleChange(event) {
-    //this.setState({vehicleType: event.target.vehicleType});
+    this.setState({ticketMessage: event.target.value});
+    console.log("Change: " + event.target.value);
   }
 
   handleSubmit(event) {
-    //alert('YOUR VEHICLE TYPE: ' + this.state.vehicleType);
+    // alert('A name was submitted: ' + this.state.value);
+    console.log("Submit: " + this.state.ticketMessage);
     event.preventDefault();
-  }
 
-  sendEmail(e){
-    e.preventDefault();
-
-    emailjs.sendForm('service_gy667yu', 'template_py26dfq', e.target, 'user_VZ5P3vcuhc1OYWRMpNR1h')
+    emailjs.sendForm('service_gy667yu', 'template_py26dfq', event.target, 'user_VZ5P3vcuhc1OYWRMpNR1h')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
-    e.target.reset()
-}
+      event.target.reset()
+  }
 
   render() {
-      return (
-        <div className="">
-          <div class="box">
+    return (
+      <div>
+        <div class="box">
           <h1 class="title is-1">Customer Ticker Creator</h1>
-          </div>
-          <div>
-            <br></br>
-        <form onSubmit={this.sendEmail}>
-
-            <div class="field">
+        </div>
+        <div>
+          <br></br>
+          <form onSubmit={this.handleSubmit}>
+              <div class="field">
                 <label class="label">Name</label>
                 <div class="control">
                     <input className="form-control has-text-centered input" type="text" name="name" placeholder="Name" />
                 </div>
-            </div>
+              </div>
 
-            <div class="field">
+              <div class="field">
                 <label class="label">Email</label>
                 <div class="control has-icons-left has-icons-right">
                     <input className="form-control has-text-centered input" type="email" placeholder="Email Address" defaultValue="" name="email"/>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                        <i class="fas fa-exclamation-triangle"></i>
-                    </span>
                 </div>
-            </div>
+              </div>
 
-            <div class="field">
+              <div class="field">
                 <label class="label">Message</label>
                 <div class="control">
-                      <textarea className="form-control has-text-centered textarea" id="" cols="30" rows="8" placeholder="Message" name = "message" defaultValue=""></textarea>
+                      <textarea className="form-control has-text-centered textarea" id="" cols="30" rows="8" placeholder="Message" name="message" value={this.state.ticketMessage} onChange={this.handleChange} defaultValue=""></textarea>
                 </div>
-            </div>
+              </div>
 
-          
-
-
-
-            <br></br>
-            <div class="field">
+              <div class="field">
                 <div class="control">
                     <input type = "submit" className="btn btn-info has-text-centered button is-link" value="Submit Ticket"/>
                 </div>
-            </div>
-
-
-        </form>
-
-    </div>
+              </div>
+          </form>
         </div>
-      );
-    }
-} 
+    </div>
+      
+    );
+  }
+}
 
 export default Questions;
